3b8a21d092a3e701231002ad1a4c7df3b11177e6
Fabio Eduardo Furquim <59511753+furquim99@users.noreply.github.com>
2022-06-23 15:33:33 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,92 +1,189 @@
-
-import React, { useState ,useEffect } from "react";
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 import Axios from "axios";
-import Card from "./components/cards/card";
+//import { Form } from "react-bootstrap";
+//import { Button } from "react-bootstrap"; css avançado no udemy anotaçao
+//import AppLogin from "../src/login/AppLogin"
 
-export default function App() {
-const [values, setValues] = useState();
-const [listCard, setListCard] = useState([]);
-  console.log(listCard);
+//import { BrowserRouter as Router, Route } from "react-router-dom";
 
-  const handleRegisterPost = () => {
-    Axios.post("http://localhost:3001/register", {
-      nome: values.nome,
-      titulo: values.titulo,
-      descricao: values.descricao,
-    }).then(() => {
-      Axios.post("http://localhost:3001/search", {
-        nome: values.nome,
-        titulo: values.titulo,
-        descricao: values.descricao,
-      }).then((response) => {
-        setListCard([
-          ...listCard,
-          {
-            id: response.data[0].id,
-            nome: values.nome,
-            titulo: values.titulo,
-            descricao: values.descricao,
-          },
-        ]);
-      });
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+  const [newWage, setNewWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
     });
   };
-  useEffect(() => {
-    Axios.get("http://localhost:3001/getCards").then((response) => {
-      setListCard(response.data);
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
     });
-  }, []);
-const handleaddValues = (value) => {
-  setValues((prevValues) => ({
-    ...prevValues,
-    [value.target.name]: value.target.value,
-  }));
-};
+  };
+
+  const updateEmployeeWage = (id) => {
+    Axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
+      (response) => {
+        setEmployeeList(
+          employeeList.map((val) => {
+            return val.id === id
+              ? {
+                  id: val.id,
+                  name: val.name,
+                  country: val.country,
+                  age: val.age,
+                  position: val.position,
+                  wage: newWage,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id !== id;
+        })
+      );
+    });
+  };
 
   return (
-    <div className="app-container">
-      <div className="register-container">
-      <h1 className="register-title">Meu blog de notas</h1>
-      <input
-          type="text"
-          name="name"
-          placeholder="Nome"
-          className="register-input"
-          onChange={handleaddValues}
-        />
-        <input
-          type="text"
-          placeholder="titulo"
-          name="titulo"
-          className="register-input"
-          onChange={handleaddValues}
-        />
-        <input
-          type="text"
-          placeholder="Descricao"
-          name="descricao"
-          className="register-input"
-          onChange={handleaddValues}
-        />
-
-        <button onClick={handleRegisterPost} className="register-button">
-          Cadastrar
-        </button>
+    <div className="App container">
+      <h1>Blog Descriçoes Profissionais</h1>
+      {/*<Router>
+      <Route path="/login" render={(props) => <AppLogin/>} />
+      </Router>*/}
+      {/*  <a href="/login">Login</a>
+        <a href="/register">Criar conta</a>*/}
+      <div className="information">
+        <form action="">
+          <div className="mb-3">
+            <label className="form-label" htmlFor="name">
+              Name:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Coloque seu nome"
+              onChange={(event) => {
+                setName(event.target.value)
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="age">Idade:</label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Coloque sua idade"
+              onChange={(event) => {
+                setAge(event.target.value)
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="country">País:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Coloque seu país de origem"
+              onChange={(event) => {
+                setCountry(event.target.value)
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Position">Profissão:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Coloque sua profissão"
+              onChange={(event) => {
+                setPosition(event.target.value)
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Wage">Descrição:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Escreva aqui"
+              onChange={(event) => {
+                setWage(event.target.value)
+              }}
+            />
+          </div>
+          <button onClick={addEmployee} class="btn btn-success">
+            Adicionar Post
+          </button>
+        </form>
       </div>
-      {listCard.map((val) => (
-        <Card
-          listCard={listCard}
-          setListCard={setListCard}
-          key={val.id}
-          id={val.id}
-          name={val.name}
-          titulo={val.titulo}
-          descricao={val.descricao}
-        />
-      ))}
+      <hr />
+      <div className="employees">
+        <button class="btn btn-primary" onClick={getEmployees}>
+          Mostrar Posts
+        </button>
+        <br />
+        <br />
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee card">
+              <div className="card-body text-left">
+                <p className="card-text">Nome: {val.name}</p>
+                <p className="card-text">Idade: {val.age}</p>
+                <p className="card-text">País: {val.country}</p>
+                <p className="card-text">Profissão: {val.position}</p>
+                <p className="card-text">Descrição: {val.wage}</p>
+                <div className="d-flex">
+                  <input
+                    className="form-control"
+                    style={{ width: "300px" }}
+                    type="text"
+                    placeholder=""
+                    onChange={(event) => {
+                      setNewWage(event.target.value)
+                    }}
+                  />
+                  <button className="btn btn-warning" onClick={() => {updateEmployeeWage(val.id)}}>Editar</button>
+
+                  <button className="btn btn-danger" onClick={() => {deleteEmployee(val.id)}}>Deletar </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-
-
 }
+
+export default App;
